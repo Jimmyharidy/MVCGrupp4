4eece4339c75dcf6557e3617d54c2657d2d9ccd5
Jimmyharidy <jimmy.haridy@hotmail.com>
2016-04-26 16:32:15 +0200

    Test

---
+++ Grupp4/Grupp4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Grupp4.MvcApplication" Language="C#" %>
---
+++ Grupp4/Grupp4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grupp4.Startup))]
+namespace Grupp4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
