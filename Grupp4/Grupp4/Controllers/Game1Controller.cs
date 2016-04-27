9bf8bea993f218b750ee0bebaf561ed26849ea9e
Jimmyharidy <jimmy.haridy@hotmail.com>
2016-04-27 16:27:55 +0200

    Game21 code added

---
+++ Grupp4/Grupp4/Controllers/Game1Controller.cs
@@ -0,0 +1,66 @@
+﻿using Grupp4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grupp4.Controllers
+{
+    public class Game1Controller : Controller
+    {
+        // GET: Game1
+        public ActionResult Index()
+
+        {
+
+            var model = new Game1Model();
+
+            model.Start();
+
+
+
+            return View(model);
+
+        }
+
+
+
+
+        [HttpPost]
+
+        public ActionResult Index(Game1Model model)
+
+        {
+
+            if (model.PlayerIsWinner())
+
+            {
+
+                ViewBag.Result = "You win.";
+
+
+
+
+
+
+            }
+
+            else if (model.ComputerIsWinner())
+
+            {
+
+                ViewBag.Result = "Computer wins.";
+
+
+
+            }
+
+            ModelState.Remove("CurrentValue");
+
+            return View(model);
+
+        }
+
+    }
+}
---
+++ Grupp4/Grupp4/Models/Game1Model.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Grupp4.Models
+{
+    public class Game1Model
+    {
+        private int currentValue;
+        public int CurrentValue
+        {
+            get { return currentValue; }
+            set
+            {
+                if (currentValue > 21)
+                {
+                    currentValue = 0;
+                }
+                else
+                    currentValue = value;
+            }
+        }
+        public int ChoosenNumber { get; set; }
+        public int Computer { get; set; }
+        public bool PlayerIsWinner()
+        {
+            if (CurrentValue >= 21)
+            {
+                return true;
+            }
+
+            CurrentValue += ChoosenNumber;
+            return false;
+        }
+        public bool ComputerIsWinner()
+        {
+            if (CurrentValue >= 21)
+            {
+                return true;
+            }
+            ComputerLogic();
+            CurrentValue += Computer;
+            return false;
+        }
+        public int ComputerLogic()
+        {
+            if (CurrentValue % 3 == 0)
+            {
+               Computer += 1;
+            }
+            else if (CurrentValue % 3 == 1)
+            {
+                Computer += 2;
+            }
+            else if (CurrentValue % 3 == 2)
+            {
+                Computer += 1;
+            }
+            return Computer;
+        }
+        public void Start()
+        {
+            CurrentValue = 0;
+        }
+    }
+}
