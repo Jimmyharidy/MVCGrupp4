5e0b1a4b678951255a5574559f4fffaa3a033146
Jon Sverrisson <j_sverrisson@hotmail.com>
2016-05-10 09:22:17 +0200

    ...

---
+++ Grupp4/FiveInRow/Controllers/FiveController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FiveInRow.Models;
+
+namespace FiveInRow.Controllers
+{
+    public class FiveController : Controller
+    {
+        // GET: Five
+        public ActionResult Index()
+        {
+            FiveRow fiveRow = new FiveRow();
+            this.Session["Five"] = fiveRow;
+
+            return View(fiveRow);
+        }
+
+        [HttpPost]
+        public ActionResult Index(int? choice)
+        {
+            var fiveRow = (FiveRow)this.Session["Five"];
+            //fiveRow.Start(choice.Value);
+
+            return View(fiveRow);
+        }
+    }
+}
---
+++ Grupp4/FiveInRow/Models/FiveRow.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Web.UI.WebControls;
+
+namespace FiveInRow.Models
+{
+    public class FiveRow
+    {
+        public int[,] Board { get; set; } = new int[7,6];
+        //Alternativ
+        //public Button[,] Buttons { get; set; } = new Button[7,6];
+        public string Color { get; set; } = "";
+        public string PlayerColor { get; set; } = "blue";
+        public string ComputerColor { get; set; } = "red";
+
+        public string ChangeColor(int i, int j)
+        {
+            if (i == 1 && j == 1)
+            {
+                Color = PlayerColor;
+                return Color;
+            }
+
+
+            return "";
+        }
+
+    }
+}
